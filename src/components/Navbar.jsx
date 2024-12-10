87bdeb6989f21f37817b6f74b4b86fe11abd513e
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2024-12-10 17:18:09 +0200

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <header className="header">
+      <NavLink />
+    </header>
+  )
+}
+
+export default Navbar;
