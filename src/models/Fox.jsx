8ec0b42dfe7df048a04fab04c723fed87017b56c
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2024-12-13 17:00:34 +0200

    0.5

--- src/models/Fox.jsx
+++ src/models/Fox.jsx
@@ -1,26 +1,12 @@
-/**
- * IMPORTANT: Loading glTF models into a Three.js scene is a lot of work.
- * Before we can configure or animate our model’s meshes, we need to iterate through
- * each part of our model’s meshes and save them separately.
- *
- * But luckily there is an app that turns gltf or glb files into jsx components
- * For this model, visit https://gltf.pmnd.rs/
- * And get the code. And then add the rest of the things.
- * YOU DON'T HAVE TO WRITE EVERYTHING FROM SCRATCH
- */
-
 import React, { useRef, useEffect } from "react";
 import { useGLTF, useAnimations } from "@react-three/drei";
-
 import scene from "../assets/3d/fox.glb";
 
-// 3D Model from: https://sketchfab.com/3d-models/fox-f372c04de44640fbb6a4f9e4e5845c78
-export function Fox({ currentAnimation, ...props }) {
+const Fox = ({ currentAnimation, ...props }) => {
   const group = useRef();
   const { nodes, materials, animations } = useGLTF(scene);
   const { actions } = useAnimations(animations, group);
 
-  // This effect will run whenever the currentAnimation prop changes
   useEffect(() => {
     Object.values(actions).forEach((action) => action.stop());
 
@@ -69,3 +55,4 @@ export function Fox({ currentAnimation, ...props }) {
 }
 
 useGLTF.preload(scene);
+export default Fox;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,19 +1,64 @@
-import React, {useState} from "react";
+import React, { useState, useRef, Suspense } from "react";
+import emailjs from '@emailjs/browser';
+import { Canvas } from '@react-three/fiber';
+import Loader from "../components/Loader";
+import Fox from '../models/Fox';
+import useAlert from '../hooks/useAlert';
 
 const Contact = () => {
+  const formRef = useRef(null);
   const [form, setForm] = useState({name: '', email: '', message: ''});
-  const [isLoading, setIsloading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [currentAnimation, setCurrentAnimation] = useState('idle');
+  
+  const { alert, showAlert, hideAlert} = useAlert();
 
-  const handleChange = () => {};
-  const handleFocus = () => {};
-  const handleBlur = () => {};   
+  const handleChange = (event) => {
+    setForm({...form, [event.target.name]: event.target.value})
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setIsLoading(true);
+    setCurrentAnimation('hit');
+
+    emailjs.send(
+      import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+      import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+      {
+        from_name: form.name,
+        to_name: "Dmitrii",
+        from_email: form.email,
+        to_email: 'magistrigusti@gmail.com',
+        message: form.message
+      },
+      import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+    ).then(() => {
+      setIsLoading(false);
+
+      setTimeout(() => {
+        setCurrentAnimation('idle');
+        setForm({name: '', email: '', message: ''});
+      }, [3000]);
+    }).catch((error) => {
+      setIsLoading(false);
+      setCurrentAnimation('idle');
+      console.log(error);
+    })
+  };
+
+  const handleFocus = () => setCurrentAnimation('walk');
+  const handleBlur = () => setCurrentAnimation('idle');   
+  
 
   return (
     <section className="relative flex l:flex-row flex-col max-container">
       <div className="flex-1 min-w-[50%] flex flex-col">
         <h1 className="head-text">Get in Touch</h1>
 
-        <form className="w-full flex flex-col gap-7 mt-14">
+        <form className="w-full flex flex-col gap-7 mt-14"
+          onSubmit={handleSubmit}
+        >
           <label className="text-black-500 font-semibold">
             Name
             <input className="input" 
@@ -21,7 +66,7 @@ const Contact = () => {
               type="text" 
               name="name" 
               required
-              value={form.name}
+              defaultValue={form.name}
               onChange={handleChange}
               onFocus={handleFocus}
               onBlur={handleBlur}
@@ -35,7 +80,7 @@ const Contact = () => {
               name="email"
               placeholder="john@gmail.com"
               required
-              value={form.email}
+              defaultValue={form.email}
               onChange={handleChange}
               onFocus={handleFocus}
               onBlur={handleBlur}
@@ -44,13 +89,13 @@ const Contact = () => {
 
           <label className="text-black-500 font-semibold">
             Your Message
-            <textarea placeholder="Let me know how I can help you!"
+            <textarea placeholder="Write your thoughts here...!"
               name="message"
-              rows={4}
+              rows="4"
               className="textarea"
               required
-              value={form.message}
-              onChahge={handleChange}
+              defaultValue={form.message}
+              onChange={handleChange}
               onFocus={handleFocus}
               onBlur={handleBlur}
             />
@@ -65,6 +110,27 @@ const Contact = () => {
             {isLoading ? 'Sending...' : 'Send Message'}
           </button>
         </form>
+      </div>
+
+      <div className="lg:w-1/2 w-full lg:h-auto md:h-[550px] h-[350px]">
+        <Canvas
+          camera={{position: [0, 0, 5],
+            fov: 75,
+            near: 0.1,
+            far: 1000
+          }}
+        >
+          <directionalLight intensity={2.5} position={[0, 0, 1]} /> 
+          <ambientLight intensity={0.5} />
+
+          <Suspense fallback={<Loader />}>
+            <Fox currentAnimation={currentAnimation}
+              position={[0.5, 0.35, 0]}
+              rotation={[12.6, -0.6, 0]}
+              scale={[0.5, 0.5, 0.5]}
+            />
+          </Suspense>
+        </Canvas>
       </div>
     </section>
   )
