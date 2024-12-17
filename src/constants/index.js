5a3486551c67455cc6be4f50882e12c7f0be41b1
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2024-12-17 10:14:13 +0200

    0.8a

--- src/constants/index.js
+++ src/constants/index.js
@@ -189,7 +189,7 @@ export const projects = [
         theme: 'btn-back-red',
         name: 'Amazon Price Tracker',
         description: 'Developed a web application that tracks and notifies users of price changes for products on Amazon, helping users find the best deals.',
-        link: 'https://github.com/adrianhajdin/pricewise',
+        link: 'https://github.com/magistrigusti/MarketplacePublic.git',
     },
     {
         iconUrl: threads,
